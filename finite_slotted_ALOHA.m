62c0b8b593d715902c0cbe84fa5cb835549dfff2
Tim Chen <75072997+Timchen0111@users.noreply.github.com>
2024-10-03 15:28:00 +0800

    Complete SA in Msg3-EDT transmission (Not considered channel model)

--- finite_slotted_ALOHA.m
+++ finite_slotted_ALOHA.m
@@ -1,44 +1,52 @@
 clear all; close all; clc;
-sourceNumber = 10;
-carrier_num = 1;
+sourceNumber = 150;
+N_sc = 48;
+N_EDT = 3;
 %carrier_state = zeros(1,carrier_num); %Boolean
-%Adapt QPSK
+%Traffic generation
+rate = 0.61;%FEC coding rate
+M = 4;%Modulation: QPSK
+N_pk = 0;%Average number of packets, related to G
+T_EDT = 1;%Single EDT duration
+packet_bit =328;%Packet bit length
 count=1;
-simulationTime=1e5;
-EDT_occasion_num = 1;
+simulationTime=1e4;
 %total_num = EDT_occasion_num*carrier_num;
 %需重新考慮traffic load 計算方式
 %Test for different number of carrier
-for lambda=[0:0.2:8] %Simulation for different traffic load.
-%lambda = 2;
-%for carrier_num = 1:30
+%for lambda=[0:0.2:8] %Simulation for different traffic load.
+for G= 0:1:90 %Simulation for different traffic load. Unit: kbps
+        %Generate Poisson arrival
+        G_ = G*01000;
+        N_pk = G_*T_EDT/packet_bit;
+        lambda = N_pk;%Calculate average number of packets
         sourceStatus = zeros(1,sourceNumber);
         Record_carrier = zeros(1,sourceNumber);
         Record_EDT = zeros(1,sourceNumber);
         % 0: idle source
         % 1: active
-        attemptSource = zeros(EDT_occasion_num,carrier_num);
+        attemptSource = zeros(N_EDT,N_sc);
         pcktTransmissionAttempts = 0;
         %nodeDelay = zeros(1, sourceNumber);
         %sumDelay=0;
         ackdPacketCount = 0;
         pcktCollisionCount = 0;
         burstSlot = 0;
-        pr=lambda/sourceNumber; %此行導致throughput不隨UE量增加，因throughput只受lambda影響
+        pr = 1 - exp(-lambda/sourceNumber);  % 根據 Poisson 分佈來計算傳輸機率 此行導致throughput不隨UE量增加，因throughput只受lambda影響
         %fileID = fopen('lambda5.txt','w');
         time = 0;
         while time < simulationTime
             time = time+1;%Time pass by
             %fprintf(fileID, 'slot = %i \n'
             % , currentSlot);
-            transmissionAttemptsEachSlot = zeros(EDT_occasion_num,carrier_num);
+            transmissionAttemptsEachSlot = zeros(N_EDT,N_sc);
             %Memoryless Bernoulli trial (May need modification)
             for source = 1:sourceNumber %For each UE
                 if sourceStatus(source) == 0 && rand(1) <= pr % new packet coming
-                    sourceStatus(source) = 1;
+                    sourceStatus(source) = 1; 
                     %nodeDelay(source)=0;
-                    Chosen_carrier = randi(carrier_num);
-                    Chosen_EDT = randi(EDT_occasion_num);
+                    Chosen_carrier = randi(N_sc);
+                    Chosen_EDT = randi(N_EDT);
                     Record_carrier(source) = Chosen_carrier;
                     Record_EDT(source) = Chosen_EDT;
                     transmissionAttemptsEachSlot(Chosen_EDT,Chosen_carrier) = transmissionAttemptsEachSlot(Chosen_EDT,Chosen_carrier)+1;
@@ -57,19 +65,21 @@ for lambda=[0:0.2:8] %Simulation for different traffic load.
                     end
                 end
             end
-            for i = 1:EDT_occasion_num
-                for j = 1:carrier_num
+            for i = 1:N_EDT
+                for j = 1:N_sc
                     if transmissionAttemptsEachSlot(i,j) == 1 %No collision
-                        burstSlot = burstSlot + 1; 
                         ackdPacketCount = ackdPacketCount + 1;
                         %sumDelay = sumDelay+nodeDelay(attemptSource);
                         sourceStatus(attemptSource(i,j)) = 0;
                         Record_EDT(attemptSource(i,j)) = 0;
                         Record_carrier(attemptSource(i,j)) = 0;
                         %fprintf(fileID, 'station %d packet is successfull with delay %d \n', attemptSource, nodeDelay(attemptSource));
-                    elseif transmissionAttemptsEachSlot(i,j)>1 %Collision happens
-                        burstSlot = burstSlot + 1; 
+                    elseif transmissionAttemptsEachSlot(i,j)>1 %Collision happens 
                         pcktCollisionCount = pcktCollisionCount+1;
+                        %Now, no retransmission
+                        sourceStatus(attemptSource(i,j)) = 0;
+                        Record_EDT(attemptSource(i,j)) = 0;
+                        Record_carrier(attemptSource(i,j)) = 0;
                         %fprintf(fileID, 'COLLISION Happens \n');
                     end
                 end
@@ -77,65 +87,35 @@ for lambda=[0:0.2:8] %Simulation for different traffic load.
             
         end
         
-        trafficOffered(1,count) = pcktTransmissionAttempts / time;
+        %trafficOffered(1,count) = pcktTransmissionAttempts / time;
         %     if ackdPacketCount == 0
         %         meanDelay = simulationTime; % theoretically, if packets collide continously, the delay tends to infinity
         %     else
         %         meanDelay = sumDelay/ackdPacketCount;
         %     end
-        allslot = time*EDT_occasion_num*carrier_num;
-        throughput(1,count) = ackdPacketCount / allslot;
-        pcktCollisionProb(1,count) = pcktCollisionCount / burstSlot;
+        Collision_prob(1,count) = pcktCollisionCount / (ackdPacketCount+pcktCollisionCount);
         %fclose(fileID);
         count=count+1
 end
-
+load = 0:1:90;
+throughput = load.*(1-Collision_prob);
 %% plot
 figure(1)
-carrier_number = 1:30;
-plot(trafficOffered(1,:),throughput(1,:), '-x')
+plot(load(1,:),throughput(1,:), '-x')
 %plot(carrier_number(1,:),throughput(1,:), '-x')
 title('Average Throughput of Finite-Station Slotted ALOHA')
 xlabel('G (Offered Traffic)')
 %xlabel('number of frequency carriers')
 ylabel('Average Throughput')
 hold on;
-G=[0:0.2:8];
-%S=G.*(1-G/10).^(10-1);
-%plot(G,S);
-%hold on;
-%plot(trafficOffered(2,:),throughput(2,:), '-s')
-%hold on;
-%S=G.*(1-G/25).^(25-1);
-%plot(G,S);
-%hold on;
-%plot(trafficOffered(3,:),throughput(3,:), '-o')
-%hold on;
-%S=G.*(1-G/50).^(50-1);
-%plot(G,S);
-%legend('simulation, M=10', 'analytical, M=10','simulation, M=25', 'analytical, M=25', 'simulation, M=50', 'analytical, M=50');
 grid on
-
+%Collision_prob = log10(Collision_prob);
 figure(2)
-plot(trafficOffered(1,:),pcktCollisionProb(1,:), '-x')
-%plot(carrier_number(1,:),pcktCollisionProb(1,:), '-x')
+plot(load(1,:),Collision_prob(1,:), '-x')
 title('Collision Probability of Finite-Station Slotted ALOHA')
 xlabel('G (Offered Traffic)')
 %xlabel('number of frequency carriers')
+set(gca, 'YScale', 'log');
 ylabel('Collision Prob')
 hold on;
-G=[0:0.2:8];
-%S=1-G.*(1-G/10).^(10-1)-(1-G/10).^(10);
-%plot(G,S);
-%hold on;
-%plot(trafficOffered(2,:),pcktCollisionProb(2,:), '-s')
-%hold on;
-%S=1-G.*(1-G/25).^(25-1)-(1-G/25).^(25);
-%plot(G,S);
-%hold on;
-%plot(trafficOffered(3,:),pcktCollisionProb(3,:), '-o')
-%hold on;
-%S=1-G.*(1-G/50).^(50-1)-(1-G/50).^(50);
-%plot(G,S);
-%legend('simulation, M=10', 'analytical, M=10','simulation, M=25', 'analytical, M=25', 'simulation, M=50', 'analytical, M=50');
 grid on
